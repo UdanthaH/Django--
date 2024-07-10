b9b0ad62dde15b25290ceabfa969e1bc17aaec07
UdanthaH <uherath@ivytech.edu>
2024-07-09 23:34:40 -0400

    M05 deploy and view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post  # Import your Post model
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()  # Query your Post objects
+    return render(request, 'blog/post_list.html', {'posts': posts})
